be43607f6ce386c0457f2bab354c15dd3f11830d
Maria <julianahenao1510@gmail.com>
2025-07-06 20:32:03 -0500

    Agrego link para conexion de backend en Render

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5011'
+    baseURL: 'https://extrahoursbackend.onrender.com/'
 });
 
 api.interceptors.request.use((config)=>{
